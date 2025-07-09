ffdb679edfe997fc864f37320bd11625e001b2d9
Alejadro <jasielalejandrolugodzib@gmail.com>
2025-07-08 21:46:06 -0600

    quiza jala

--- config/db.ts
+++ config/db.ts
@@ -12,7 +12,7 @@ export const db = new DataSource({
   username: process.env.DB_USER,
   password: process.env.DB_PASS,
   database: process.env.DB_NAME,
-  entities: [__dirname + "/../models/**/*.ts"],
+   entities: [__dirname + "/../models/**/*.{ts,js}"],
   synchronize: true,
   logging: false,
 });
